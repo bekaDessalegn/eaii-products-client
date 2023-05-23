25ef5d08328e488810ff61fd8b5ca40b1c38c45d
Beka Dessalegn <39644079+bekaDessalegn@users.noreply.github.com>
2023-05-23 21:03:08 +0300

    fixed the eslint issue

--- next.config.js
+++ next.config.js
@@ -3,7 +3,10 @@ const nextConfig = {
   reactStrictMode: true,
   env: {
     baseUrl: 'http://localhost:8080/v1/graphql'
-  }
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+},
 }
 
 module.exports = nextConfig
